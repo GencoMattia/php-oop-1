f2882ff8dd9dd863b425d658096c38855c618202
GencoMattia <gencomattia@hotmail.it>
2024-07-02 15:58:09 +0200

    created 2 movie objects and printed with the class method on page

--- index.php
+++ index.php
@@ -18,4 +18,30 @@ class Movie {
     }
 }
 
+$newMovie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, "Drammatico");
+$newMovie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, "Thriller");
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+<body>
+    <main>
+        <article>
+            <?php 
+            echo $newMovie1 -> getMovieInfo();            
+            ?>
+        </article>
+        <article>
+            <?php 
+            echo $newMovie2 -> getMovieInfo();            
+            ?>
+        </article>
+    </main>
+</body>
+</html>
