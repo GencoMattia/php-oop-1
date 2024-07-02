16db9b9bdafc08bfb2c158286ee91902919c76e5
GencoMattia <gencomattia@hotmail.it>
2024-07-02 16:02:44 +0200

    moved Movie class to Movie.php file

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
 
-class Movie {
-    public $title;
-    public $director;
-    public $year;
-    public $genre;
-
-    function __construct($title, $director, $year, $genre) {
-        $this -> title = $title;
-        $this -> director = $director;
-        $this -> year = $year;
-        $this -> genre = $genre;
-    }
-
-    public function getMovieInfo(){
-        return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}, Genere: {$this->genre}";
-    }
-}
+require_once __DIR__ . "/models/Movie.php";
 
 $newMovie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, "Drammatico");
 $newMovie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, "Thriller");
