e76adf93fc8b577063f65ad1f03e9d39159bab3e
GencoMattia <gencomattia@hotmail.it>
2024-07-02 16:12:50 +0200

    added Genre.php file with Genre class and created few new Genre objects

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
 require_once __DIR__ . "/models/Movie.php";
+require_once __DIR__ . "/models/Genre.php";
 
+// Movies
 $newMovie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, "Drammatico");
 $newMovie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, "Thriller");
+
+// Genres
+$drama = new Genre("Drammatico");
+$crime = new Genre("Crime");
+$thriller = new Genre("Thriller");
+$blackComedy = new Genre("Commedia Nera");
 
 ?>
 
